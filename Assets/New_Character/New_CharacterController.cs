3bdcacaf70bf8abbb924c1ff0abeb31e494b7213
Santiago Ruiz <sruiz557@unab.edu.co>
2026-05-06 12:43:22 -0500

    Animaciones (Jumping)

--- Assets/New_Character/New_CharacterController.cs
+++ Assets/New_Character/New_CharacterController.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -9,6 +10,7 @@ public class New_Character : MonoBehaviour
     public float jumpHeight = 2f;
     public float rotationSpeed = 10f;
     public float mouseSensitivity = 1f;
+    public float gravity = -20f;
 
     [Header("Referenciacion")]
     public Transform cameraTransform;
@@ -20,17 +22,90 @@ public class New_Character : MonoBehaviour
     private float yaw;
 
     private Vector3 externalVelocity = Vector3.zero;
+
+    public bool IsMoving { get; private set; }
+    public Vector2 CurrentInput { get; private set; }
+    public bool IsGrounded{get; private set;}
+    public float CurrentYaw => yaw;
     
 
 
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleRotation();
+        updateAnimator();
     }
+    void HandleMovement()
+    {
+        IsGrounded = characterController.isGrounded;
+        if (IsGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 inputDirection = new Vector3(horizontal, 0f, vertical).normalized;
+        IsMoving = inputDirection.magnitude > 0.1f;
+
+        Vector3 moveDirection = Vector3.zero; 
+
+        if (IsMoving)
+        {
+            moveDirection = Quaternion.Euler(0f, cameraTransform.eulerAngles.y, 0f) * inputDirection;
+            bool isSprinting = Input.GetKey(KeyCode.LeftShift);
+            currentSpeed = isSprinting ? SprintSpeed : WalkSpeed;
+        }
+
+        if (Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator?.SetBool("IsJumping", true);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        Vector3 finalmovement = (moveDirection * currentSpeed + externalVelocity);
+
+        // 👇 esto es lo importante
+        finalmovement.y = velocity.y;
+
+        characterController.Move(finalmovement * Time.deltaTime);
+
+        if(IsGrounded && velocity.y < 0f)
+        {
+            animator?.SetBool("IsJumping", false);
+        }
+    }
+
+    void HandleRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        yaw += mouseX;
+
+        if (IsMoving)
+        {
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.Euler(0f, yaw, 0f),
+                rotationSpeed * Time.deltaTime
+            );
+        }
+    }
+    void updateAnimator()
+    {
+        float SpeedPercent = IsMoving ? (currentSpeed == SprintSpeed ? 1f : 0.5f) : 0f;
+
+        animator?.SetFloat("Speed", SpeedPercent, 0.1f, Time.deltaTime);
+        animator?.SetBool("IsGrounded", IsGrounded);
+        animator?.SetFloat("VerticalSpeed", velocity.y);
+    }
+   
 }
